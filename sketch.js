1fe8a2c1849bc7c5c89b9f0106f85d951e2b90e5
GreenCat <tedd0328@gmail.com>
2022-06-01 18:22:41 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -69,7 +69,7 @@ function keyPressed() {
 
 function windmill() {
   console.log(pic.width, pic.height);
-  var tmp = new PImage(pic.width, pic.height);
+  var tmp = createImage(pic.width, pic.height);
   tmp.loadPixels();
 
   for (var row = 0; row < tmp.height; row++) {
@@ -127,7 +127,7 @@ function cyberpunk() {
 }
 
 function edgey() {
-  var tmp = new Array(pic.width, pic.height);
+  var tmp = createImage(pic.width, pic.height);
   tmp.loadPixels();
 
   for (var row = 0; row < pic.height; row++) {
@@ -172,7 +172,7 @@ function diff(c, d) {
 }
 
 function wavy() {
-  var tmp = new Array(pic.width-20, pic.height);
+  var tmp = createImage(pic.width-20, pic.height);
   tmp.loadPixels();
 
   for (var row = 0; row < tmp.height; row++) {
@@ -188,7 +188,7 @@ function wavy() {
 }
 
 function fishTunnel() {
-  var tmp = new Array(pic.width, pic.height);
+  var tmp = createImage(pic.width, pic.height);
   tmp.loadPixels();
 
   for (var y = 0; y < pic.height; y++) {
